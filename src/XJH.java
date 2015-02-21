014e7dc7e7d576070d86b5f1a4eaa085ade466d0
Shrinky <number45zephan@gmail.com>
2015-02-21 08:45:15 -0500

    Doesn't work just yet...

--- src/XJH.java
+++ src/XJH.java
@@ -10,10 +10,16 @@ import java.util.logging.Logger;
 
 public class XJH {
 
-    public static void main(String[] args) {
+    AddressConverter json;
 
+    public XJH() {
+        json = new AddressConverter();
+    }
+
+    public void main(String[] args) throws IOException {
         XJH obj = new XJH();
         obj.run();
+        json.convertToLatLong("1320 Edwards Dr. Downingtown, PA 19335");
     }
 
     public void run() {
@@ -22,42 +28,42 @@ public class XJH {
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
-            PrintWriter writer = null;
-            try {
-                writer = new PrintWriter("dataTEST.txt", "UTF-8");
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("dataTEST.txt", "UTF-8");
 
-                br = new BufferedReader(new FileReader(csvFile));
-                while ((line = br.readLine()) != null) {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
 
-                    // use comma as separator
-                    String[] data = line.split(cvsSplitBy);
+                // use comma as separator
+                String[] data = line.split(cvsSplitBy);
 
-                    writer.println();
-                    writer.println("Last Name: " + data[5]);
-                    writer.println("First name: " + data[4]);
-                    writer.println("Phone: " + data[13]);
-                    writer.println();
-                }
+                writer.println();
+                writer.println("Last Name: " + data[5]);
+                writer.println("First name: " + data[4]);
+                writer.println("Phone: " + data[13]);
+                writer.println();
+            }
 
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(XJH.class.getName()).log(Level.SEVERE, null, ex);
-                 ex.printStackTrace();
-            } catch (UnsupportedEncodingException ex) {
-                Logger.getLogger(XJH.class.getName()).log(Level.SEVERE, null, ex);
-                 ex.printStackTrace();
-            } catch (IOException ex) {
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(XJH.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(XJH.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        } catch (IOException ex) {
             Logger.getLogger(XJH.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-                if (br != null) {
-                    try {
-                        br.close();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
                 }
-                writer.close();
             }
-            System.out.println("Done");
-
+            writer.close();
         }
+        System.out.println("Done");
+
     }
+}
